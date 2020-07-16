ee4dfb18f2f7d1ba786de63369594cf99d49bdb3
Nwokochahannah <hannahsopuruchi@gmail.com>
2020-07-16 18:00:42 +0100

    error

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -5,7 +5,7 @@ $dbparts = parse_url($url);
 $hostname = $dbparts['x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
 $username = $dbparts['ybso5b8pq4se2yeo'];
 $password = $dbparts['eja2xwke89wn0st9'];
-$database = ltrim($dbparts['xo0n4ya5m71arj2d'],'/');
+$database = ltrim($dbparts['xo0n4ya5m71arj2d']);
 
 
 // Create connection
